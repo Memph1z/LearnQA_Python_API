d777da6926cefb7b95c063cccb8bb5e57c1d7394
sousFlex <memph1zgray@gmail.com>
2025-12-28 20:22:29 +0300

    Ex11

---
+++ lib/assertions.py
@@ -0,0 +1,22 @@
+from requests import Response
+import json
+
+class Assertions:
+    @staticmethod
+    def assert_json_value_by_name(response : Response, name, expected_value, error_message):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response is '{response.text}'"
+
+        assert name in response_as_dict, f"Response does not contain '{name}'"
+        assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_phrase_is_less_than_15_symbols(phrase, error_message):
+        assert len(phrase) < 15, error_message
+
+    @staticmethod
+    def assert_cookie_value(response : Response, cookie_name, cookie_value, error_message):
+        assert cookie_name in response.cookies, f"{cookie_name} not found in cookies"
+        assert response.cookies[cookie_name] == cookie_value, error_message
---
+++ lib/base_case.py
@@ -0,0 +1,24 @@
+import json
+
+from requests import Response
+class BaseCase:
+    def get_cookie(self, response : Response, cookie_name):
+        assert cookie_name in response.cookies, f"Cannot find cookie {cookie_name}"
+        return response.cookies[cookie_name]
+
+    def get_header(self, response : Response, header_name):
+        assert header_name in response.headers, f"Cannot find header {header_name}"
+        return response.headers[header_name]
+
+    def get_json_value(self, response : Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response does not contain '{name}'"
+        return response_as_dict[name]
+
+    def print_cookies(self, response : Response):
+        cookies_as_dict = dict(response.cookies)
+        print(cookies_as_dict)
