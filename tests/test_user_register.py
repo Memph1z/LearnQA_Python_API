cc4f1a799b269d37dabbe402f81e2109a258b007
sousFlex <memph1zgray@gmail.com>
2026-01-10 14:23:48 +0300

    Ex15

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,6 +1,7 @@
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import  Assertions
+import pytest
 
 class TestUserRegister(BaseCase):
 
@@ -20,3 +21,39 @@ class TestUserRegister(BaseCase):
 
         Assertions.assert_status_code(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content '{response.content}'"
+
+    def test_user_incorrect_email(self):
+        email = 'wtfisthis.example.com'
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user", data = data)
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode("utf-8") == "Invalid email format", f"Unexpected response content '{response.content}'"
+
+    def test_user_short_name(self):
+        data = self.prepare_registration_data()
+        data['firstName'] = '1'
+
+        response = MyRequests.post("/user", data = data)
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode(
+            "utf-8") == "The value of 'firstName' field is too short", f"Unexpected response content '{response.content}'"
+
+    def test_user_long_name(self):
+        data = self.prepare_registration_data()
+        data['lastName'] = 'fihitjbxeegigugymfnjzfxcjeynrzerhaeictzarnkkenuvmfraeudjtmankcxguadzuuyueiwxrmaiiwvyeztwpecwmzkwimceimpaubxjpxfjzhvqmzdvjihetjaubzavmpqeykvzxicqzuifvzxtxfhnmixmrewcwupwtewvqmptgueaqmcbvfkgrrmrqcqivnubccdrnfmugaajcaavwcvnnivkwrgiaznhrhbjkbxnhkactfizxdj'
+
+        response = MyRequests.post("/user", data = data)
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode(
+            "utf-8") == "The value of 'firstName' field is too long", f"Unexpected response content '{response.content}'"
+
+    @pytest.mark.parametrize("condition", ["password", "username", "firstName", "lastName", "email"])
+    def test_missing_data_fields(self, condition):
+        data = self.prepare_registration_data()
+        data.pop(condition)
+
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The following required params are missed: {condition}", f"Unexpected response content '{response.content}'"
